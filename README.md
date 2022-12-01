b8629f365497ffb9f336408fbf7e0ab8091eb334
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2022-12-01 13:43:04 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # se-finals-project
  
+ Example project for the react and node with typescript
+ 
+npm install
+npm run build:dev
+npm run start:dev 
+http://localhost:3000
---
+++ client/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return <div>
+    <h1>TypeScript is awesome</h1>
+    <h2>Hello World</h2>
+    </div>;
+};
+
+export default App;
