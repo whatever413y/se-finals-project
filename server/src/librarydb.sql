a69cc382aa59953837fc935ff8e62954e4f4118a
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2022-12-08 15:03:05 +0800

    commit

--- server/src/librarydb.sql
+++ server/src/librarydb.sql
@@ -2,13 +2,19 @@
 start a local mysql server in localhost
 */
 
-CREATE DATABASE librarydb;
-USE librarydb;
+CREATE DATABASE `librarydb`;
+USE `librarydb`;
 
-CREATE TABLE user;
+CREATE TABLE `librarydb`.`user` (
+  `idusers` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idusers`),
+  UNIQUE INDEX `userscol_UNIQUE` (`name`, `username` ASC) VISIBLE);
 
-CREATE TABLE book;
+CREATE TABLE `book`;
 
-CREATE TABLE genre;
+CREATE TABLE `genre`;
 
-CREATE TABLE inventory;
+CREATE TABLE `inventory`;
