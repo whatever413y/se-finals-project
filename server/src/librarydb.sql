f1f9f5f033afe8a173aaacb11bc317b129d97974
Diadem Grace Arroz <diademgracearroz@gmail.com>
2022-12-11 15:15:12 +0800

    created db book table

--- server/src/librarydb.sql
+++ server/src/librarydb.sql
@@ -12,12 +12,21 @@ CREATE TABLE `librarydb`.`user` (
   `password` VARCHAR(45) NOT NULL,
   `role` VARCHAR(11) NOT NULL,
   PRIMARY KEY (`iduser`),
-  UNIQUE INDEX `userscol_UNIQUE` (`name`, `username` ASC) VISIBLE);
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE;
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);
 
 INSERT INTO `librarydb`.`user` (`name`, `username`, `password`, `role`) VALUES ('Administrator', 'admin', 'root', 'admin');
 INSERT INTO `librarydb`.`user` (`name`, `username`, `password`, `role`) VALUES ('testname', 'testusername', 'testpassword', 'user');
 
-CREATE TABLE `book`;
+CREATE TABLE `librarydb`.`book` (
+  `idbook` INT NOT NULL AUTO_INCREMENT,
+  `isbn` VARCHAR(17) NOT NULL,
+  `bookTitle` VARCHAR(45) NOT NULL,
+  `author` VARCHAR(45) NOT NULL,
+  `genre` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idbook`),
+  UNIQUE INDEX `isbn_UNIQUE` (`isbn` ASC) VISIBLE,
+  UNIQUE INDEX `bookTitle_UNIQUE` (`bookTitle` ASC) VISIBLE);
 
 CREATE TABLE `genre`;
 
