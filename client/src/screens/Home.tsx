7b48f70a3a4a14d1c287f0b9d3f9ce9ec3ad5d4e
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2023-01-02 13:49:59 +0800

    Update Home.tsx

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,5 +1,4 @@
 import React, { useState, FormEvent, ChangeEvent } from "react";
-import React, { useState, FormEvent, ChangeEvent } from "react";
 import FormInput from "../components/form-input/FormInput";
 import Axios from 'axios'
 import { useLocation } from "react-router-dom";
