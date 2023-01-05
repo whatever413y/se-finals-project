488f3cf2d6da8b3a31ba4dc5e5955fd722be7c62
Angel Tardaguela <105347598+whatever413y@users.noreply.github.com>
2023-01-05 11:52:45 +0800

    Update Database.ts

--- server/src/config/Database.ts
+++ server/src/config/Database.ts
@@ -1,11 +1,11 @@
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: 'mysqldb',
     user:'root',
     password: 'root',
     database: 'librarydb',
-    port: 3306,
+    port: 3306
 });
 
 export default db;
